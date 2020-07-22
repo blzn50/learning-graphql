2f1a996936e1e91bd19986ff270ff24c81411229
naren <blazeraze.naren@gmail.com>
2020-07-22 15:15:52 +0545

    dealing with mutation,query, directive and fragments

--- resolvers.js
+++ resolvers.js
@@ -31,13 +31,35 @@ const users = [
   },
 ];
 
-const getUser = function (args) {};
+const getUser = function (args) {
+  const userId = args.id;
+  return users.filter((u) => u.id === userId)[0];
+};
 
-const getUsers = function (args) {};
+const getUsers = function (args) {
+  if (args.shark) {
+    const shark = args.shark;
+    return users.filter((u) => u.shark === shark);
+  } else {
+    return users;
+  }
+};
+
+const updateUser = function ({ id, name, age }) {
+  users.map((user) => {
+    if (user.id === id) {
+      user.name = name;
+      user.age = age;
+      return user;
+    }
+  });
+  return users.filter((u) => u.id === id)[0];
+};
 
 const root = {
   user: getUser,
   users: getUsers,
+  updateUser: updateUser,
 };
 
 module.exports = { root };
--- schema.js
+++ schema.js
@@ -2,15 +2,19 @@ const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
   type Person{
-    id: ID!
+    id: Int!
     name: String!
     age: Int
     shark: String
   }
   
   type Query {
-    user(id: ID!): Person!
+    user(id: Int!): Person
     users(shark: String): [Person]! 
+  }
+
+  type Mutation {
+    updateUser(id: Int!, name: String!, age: String): Person
   }
 `);
 
